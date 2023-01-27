69da537bc35bdffc4b326fbeeea73eb298bb16a7
rdeamici <rtdeamicis@gmail.com>
2023-01-27 00:03:38 -0800

    asn1 part1 complete. images saved in google drive

--- box_filtering.m
+++ box_filtering.m
@@ -1,19 +1,8 @@
-function resized = box_filtering(inputImg)
+function smaller = filterAndResize(inputImg)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 N = size(inputImg,1);
-if N > 2
-    convolved = conv2(inputImg,[1,1;1,1], 'valid');
-    smaller = convolved(1:2:end,1:2:end)/4;
-    size(smaller)
-    resized = interp2(smaller, 8, 'linear');
-%     size(resized)
-    figure, imshow(resized);
-    box_filtering(smaller);
-else
-    convolved = conv2(inputImg,[1,1;1,1], 'valid');
-    final_pixel = convolved(1:2:end,1:2:end)/4;
-    final_matrix = ones(256)*final_pixel;
-    size(final_matrix)
-    imshow(final_matrix)
-end
+convolved = conv2(inputImg,[1,1;1,1], 'valid');
+smaller = convolved(1:2:end,1:2:end)/4;
+%     size(smaller)
+%     resized = interp2(smaller,'linear', 256);
