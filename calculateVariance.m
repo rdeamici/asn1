e7f5fb5d169821ccaa8f9333f1b1e03d8b079f35
rdeamici <rtdeamicis@gmail.com>
2023-02-11 23:22:46 -0800

    calculate variance using conv2()

--- calculateVariance.m
+++ calculateVariance.m
@@ -3,12 +3,14 @@ function localVar = calculateVariance(segmented)
     % define a 3x3 window the calculate local
     % variance around each pixel
     sz = 3;
-    window = ones(sz)/sz.^2;
+    window = ones(sz);
 
     % calculate local means E[X]
-    mu = conv2(segmented, window, 'same');
+    mu = conv2(segmented,window,'same')./conv2(ones(size(segmented)),window,'same');
+
     % calculate local means of squares E[X^2]
-    II = conv2(segmented.^2, window, 'same');
+    II = conv2(segmented.^2,window,'same')./conv2(ones(size(segmented)),window,'same');
+    
     % calculate local variance E[X^2] - E[X]^2
     localVar = II-(mu.^2);
 end
